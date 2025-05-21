baee70daa79e9c600ac0a60e01ea76388bda5eca
FanFan72 <isaacjoly72@gmail.com>
2025-05-21 20:56:39 +0200

    added a bunch of things

--- tic-tac-toe/m.h
+++ tic-tac-toe/m.h
@@ -14,9 +14,18 @@ void fill_grid();
 //Structs
 
 typedef struct VECTOR2{
+
     int x;
     int y;
+
 }Vector2;
+
+typedef struct PLAYER{
+
+    Vector2 v2;
+    char sign;
+
+}Player;
 
 //variables
 
@@ -26,6 +35,7 @@ char grid[ROWS][COLS] = {
     {'7', '8', '9'}
 };
 
+void place (Player *player);
 //functions
 
 void ft_putchar(char c){
@@ -42,5 +52,37 @@ void fill_grid(){
     }
 }
 
+void check(Player *player){
+    if (grid[player->v2.x][player->v2.y] == 'O' || grid[player->v2.x][player->v2.y] == 'X'){
+        printf("emplacement already taken !");
+        place(player);
+    }
+} 
+
+void place(Player *player){
+    char c = player->sign;
+    int n = -1;
+    printf(" : ");
+    scanf("%d", &n);
+    switch(n) {
+        case 1 : player->v2.x = 0, player->v2.y = 0, check(player); break;
+        case 2 : player->v2.x = 1, player->v2.y = 0, check(player); break;
+        case 3 : player->v2.x = 2, player->v2.y = 0, check(player); break;
+        case 4 : player->v2.x = 0, player->v2.y = 1, check(player); break;
+        case 5 : player->v2.x = 1, player->v2.y = 1, check(player); break;
+        case 6 : player->v2.x = 1, player->v2.y = 2, check(player); break;
+        case 7 : player->v2.x = 2, player->v2.y = 0, check(player); break;
+        case 8 : player->v2.x = 2, player->v2.y = 1, check(player); break;
+        case 9 : player->v2.x = 2, player->v2.y = 2, check(player); break;
+    }
+    grid[player->v2.x][player->v2.y] = player->sign;
+}
+
+void init_player(){
+    Player player1;
+    Player player2;
+    player1.sign = 'X';
+    player2.sign = 'O';
+}
 
 #endif
--- tic-tac-toe/ttt.c
+++ tic-tac-toe/ttt.c
@@ -7,7 +7,12 @@
 #include "m.h"
 
 int main(){
+    Player player1;
+    Player player2;
+    player1.sign = 'X';
+    player2.sign = 'O';
     fill_grid();
+    place(&player1);
     return 0;
 }
 
