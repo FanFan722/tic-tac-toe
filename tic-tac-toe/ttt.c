0b1042e2367c1e06e91efed9a0720b003181b5a2
FanFan72 <isaacjoly72@gmail.com>
2025-05-21 18:32:12 +0200

    test

---
+++ tic-tac-toe/ttt.c
@@ -0,0 +1,15 @@
+
+#ifndef TTT
+#define TTT
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include "m.h"
+
+int main(){
+    game();
+    return 0;
+}
+
+#endif
